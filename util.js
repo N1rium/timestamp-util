6d1e7dc5e8fd6c1c7eca4366c11fca191ea05b5c
blomman <johnny@gumbler.com>
2018-03-20 22:38:44 +0100

    Fixed indentation

--- util.js
+++ util.js
@@ -1,14 +1,14 @@
 const moment = require('moment');
 const math = require('mathjs');
 
-  getCalculatedString = (str) =>
-    str.toLowerCase()
-      .replace('now', new Date().getTime())
-      .replace('ms',  '(1)')
-      .replace('s',   '(1000)')
-      .replace('m',   '(1000 * 60)')
-      .replace('h',   '(1000 * 60 * 60)')
-      .replace('d',   '(1000 * 60 * 60 * 24)');
+getCalculatedString = (str) =>
+  str.toLowerCase()
+    .replace('now', new Date().getTime())
+    .replace('ms',  '(1)')
+    .replace('s',   '(1000)')
+    .replace('m',   '(1000 * 60)')
+    .replace('h',   '(1000 * 60 * 60)')
+    .replace('d',   '(1000 * 60 * 60 * 24)');
 
 
 module.exports = {
